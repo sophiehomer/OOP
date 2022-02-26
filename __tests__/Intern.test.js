6c67246fe82883147adae5199ac1d43d16781633
sophiehomer <sophiehomer94@gmail.com>
2022-02-26 12:41:11 -0800

    intern test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,17 @@
+const Intern = require("../lib/Intern");
+
+test("creates object intern", () => {
+    const int = new Intern("Oliver", 2, "oliver@gmail.com", "Columbia")
+    expect(int.name).toBe("Oliver");
+    expect(int.id).toEqual(expect.any(Number));
+       
+    const isChar = int.email.includes("@");
+    if (isChar) {
+    console.log("email contains @")
+    } else {
+    console.log("email does not contain @")
+    }
+    
+    expect(isChar).toBe(true)
+    expect(int.school).toBe("Columbia")
+   })
