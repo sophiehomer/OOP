cd942f96edba0082f90c37e2f5cbaba7c688858d
sophiehomer <sophiehomer94@gmail.com>
2022-02-26 12:35:44 -0800

    engineer & employee tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,17 @@
 const Engineer = require("../lib/Engineer")
 
 test("creates object engineer", () => {
-    const eng = new Engineer("Bobby", 94236, "apple@apple.com", "applehub")
-    expect(eng.name).toBe("Bobby")
+ const eng = new Engineer("Bobby", 1, "bobby@apple.com", "sophiehomer")
+ expect(eng.name).toBe("Bobby");
+ expect(eng.id).toEqual(expect.any(Number));
+    
+ const isChar = eng.email.includes("@");
+ if (isChar) {
+ console.log("email contains @")
+ } else {
+ console.log("email does not contain @")
+ }
+ 
+ expect(isChar).toBe(true)
+ expect(eng.github).toBe("sophiehomer")
 })
